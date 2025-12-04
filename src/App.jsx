627bedf1e62df84ae64bbba7248891afde079aea
dachiko <dachimari9@gmail.com>
2025-12-04 21:57:28 +0200

    new proect

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import HomePage from "./pages/HomePage";
+import PageNotFound from "./pages/PageNotFound";
 
 export default function App() {
-  return <div>Hello</div>;
+  return (
+    <div>
+      <h1>WorldWise</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="Product" element={<Product />} />
+          <Route path="Pricing" element={<Pricing />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
