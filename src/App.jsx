74344b5f23a1ad1a3b53411a35d980d71f63be70
dachiko <dachimari9@gmail.com>
2025-12-06 21:52:53 +0200

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
 import HomePage from "./pages/HomePage";
 import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 export default function App() {
   return (
     <div>
-      <h1>WorldWise</h1>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="Product" element={<Product />} />
           <Route path="Pricing" element={<Pricing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="app" element={<AppLayout />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PageNav from "../components/PageNav";
+import AppNav from "../components/AppNav";
+
+export default function AppLayout() {
+  return (
+    <div>
+      <AppNav />
+      <p>App</p>
+    </div>
+  );
+}
