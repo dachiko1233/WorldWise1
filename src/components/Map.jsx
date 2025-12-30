9d51713b3c76c473ea4ae208b2cdceb1d78d4061
dachiko1233 <dachimari9@email.com>
2025-12-30 13:26:15 +0200

    initial commit

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,20 +1,30 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
-import Styles from "./Map.module.css";
+import { useNavigate, useSearchParams } from 'react-router-dom';
+import Styles from './Map.module.css';
 function Map() {
-  const navigate =useNavigate()
+  const navigate = useNavigate();
   const [searchParams, setSearchParams] = useSearchParams();
-  const lat = searchParams.get("lat");
-  const lng = searchParams.get("lng");
+  const lat = searchParams.get('lat');
+  const lng = searchParams.get('lng');
   return (
-    <div className={Styles.mapContainer} onClick={() =>{navigate("form")}} >
+    <div
+      className={Styles.mapContainer}
+      onClick={() => {
+        navigate('form');
+      }}
+    >
       <h1>map</h1>
       <h1>
-        
         position
         {lat} , {lng}
       </h1>
 
-      <button onClick={() => {setSearchParams({lat:23 , lng:42})}} >Change pos</button>
+      <button
+        onClick={() => {
+          setSearchParams({ lat: 23, lng: 42 });
+        }}
+      >
+        Change pos
+      </button>
     </div>
   );
 }
